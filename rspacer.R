babee075e2b786c0bc4650b5a63bbc47c0abe45d
Takao Yokoyama <t_yokoyama@jras.co.jp>
2025-12-06 16:33:09 +0900

    add screenshot

--- rspacer.R
+++ rspacer.R
@@ -7,17 +7,16 @@ install.packages("tibble")
 # usethis を使って環境変数を設定します
 usethis::edit_r_environ()
 library(rspacer)
-
+library(tibble)
 
 
 
 # load
 library(rspacer)
 
+rspacer::set_api_url()
 
-rspacer::set_api_url("http://rspace.gmo-rast.co.jp/api/v1")
 api_status()
-HyWDGMUY20LMTU31MpWs5Vx70cugPJtU
 
 (file_name <- system.file("Template_example.html", package = "rspacer"))
 
@@ -27,23 +26,23 @@ stopifnot(file.exists(file_name))
 stopifnot(file.exists(matching_code_file))
 
 folder_tree()
-# folder_tree(180)
-# TemplatesフォルダのIDを指定しても何故か機能しない
+folder_tree("FL180")
 
 df_to_upload <- data.frame(
   name = c("Title", "Date", "Main part", "Conclusion"),
   content = c("example title", "23-12-2024", "This is example text.",
               "ready for uploading.")
 )
+
 document_create_from_tabular(
   df = df_to_upload,
-#  template_id = "SD367"
+  template_id = "SD397"
 )
 
 document_create_from_html(
   path = file_name,
   template_id = "SD397",
-  folder_id = "FL332",
+  folder_id = "FL416",
   tags = c("tutorial"),
   attachments = tibble(field = 3, path = matching_code_file),
   existing_document_id = NULL
